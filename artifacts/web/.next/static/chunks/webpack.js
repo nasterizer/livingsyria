5ef81c0f67c9968838c69225a9a2a89b758a3abf
nasterizer <38555291-nasterizer@users.noreply.replit.com>
2026-06-20 18:56:53 +0000

    Git commit prior to merge

--- artifacts/web/.next/static/chunks/webpack.js
+++ artifacts/web/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e5b0f2ee9e845f78"; }
+/******/ 		__webpack_require__.h = function() { return "4b85a8a886019327"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- artifacts/web/.next/static/webpack/webpack.8d0b871e86151b5f.hot-update.js
+++ artifacts/web/.next/static/webpack/webpack.8d0b871e86151b5f.hot-update.js
@@ -9,9 +9,26 @@
  */
 self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/compat get default export */
+/******/ !function() {
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function() { return module['default']; } :
+/******/ 			function() { return module; };
+/******/ 		__webpack_require__.d(getter, { a: getter });
+/******/ 		return getter;
+/******/ 	};
+/******/ }();
+/******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e5b0f2ee9e845f78"; }
+/******/ 	__webpack_require__.h = function() { return "4b85a8a886019327"; }
+/******/ }();
+/******/ 
+/******/ /* webpack/runtime/nonce */
+/******/ !function() {
+/******/ 	__webpack_require__.nc = undefined;
 /******/ }();
 /******/ 
 /******/ }
